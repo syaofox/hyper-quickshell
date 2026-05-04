ab5e6bf0e3ff6d8e6bf6f2dd64804e09ab49ec32
syaofox <syaofox@gmail.com>
2026-05-05 01:00:14 +0800

    123

--- dotfiles/.local/bin/power-menu.sh
+++ dotfiles/.local/bin/power-menu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pgrep -f 'quickshell.*powermenu' | grep -v $$ > /dev/null || quickshell --config ~/quickshell/powermenu/
+pgrep -f 'quickshell.*powermenu' | grep -v $$ > /dev/null || quickshell --config ~/.config/quickshell/powermenu/
